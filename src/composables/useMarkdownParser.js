0d0125392bd50a2d044f5635d3b396c14af7fa71
brian.mulier <bmmulier@hotmail.fr>
2024-09-27 11:14:01 +0200

    fix: shiki langs import are static as dynamic don't work

--- src/composables/useMarkdownParser.js
+++ src/composables/useMarkdownParser.js
@@ -1,5 +1,33 @@
 import {createMarkdownParser, createShikiHighlighter, rehypeHighlight,} from "@nuxtjs/mdc/runtime";
 import GithubDark from "shiki/themes/github-dark.mjs";
+import Bash from "shiki/langs/bash.mjs";
+import C from "shiki/langs/c.mjs";
+import Cpp from "shiki/langs/cpp.mjs";
+import Csv from "shiki/langs/csv.mjs";
+import Dockerfile from "shiki/langs/dockerfile.mjs";
+import Go from "shiki/langs/go.mjs";
+import Groovy from "shiki/langs/groovy.mjs";
+import Handlebars from "shiki/langs/handlebars.mjs";
+import Hcl from "shiki/langs/hcl.mjs";
+import Ini from "shiki/langs/ini.mjs";
+import Java from "shiki/langs/java.mjs";
+import Javascript from "shiki/langs/javascript.mjs";
+import Json from "shiki/langs/json.mjs";
+import Markdown from "shiki/langs/markdown.mjs";
+import Mermaid from "shiki/langs/mermaid.mjs";
+import Perl from "shiki/langs/perl.mjs";
+import Php from "shiki/langs/php.mjs";
+import Python from "shiki/langs/python.mjs";
+import R from "shiki/langs/r.mjs";
+import Ruby from "shiki/langs/ruby.mjs";
+import Rust from "shiki/langs/rust.mjs";
+import Scala from "shiki/langs/scala.mjs";
+import Sql from "shiki/langs/sql.mjs";
+import Systemd from "shiki/langs/systemd.mjs";
+import Twig from "shiki/langs/twig.mjs";
+import Typescript from "shiki/langs/typescript.mjs";
+import Xml from "shiki/langs/xml.mjs";
+import Yaml from "shiki/langs/yaml.mjs";
 
 export default function useMarkdownParser() {
     let parser;
@@ -8,46 +36,43 @@ export default function useMarkdownParser() {
         if (!parser) {
             const {bundledLanguagesInfo} = await import("shiki/langs");
 
-            const langsMap = {};
-            const langs = [
-                "bash",
-                "c",
-                "cpp",
-                "csv",
-                "dockerfile",
-                "go",
-                "groovy",
-                "handlebars",
-                "hcl",
-                "ini",
-                "java",
-                "javascript",
-                "json",
-                "markdown",
-                "mermaid",
-                "perl",
-                "php",
-                "python",
-                "r",
-                "ruby",
-                "rust",
-                "scala",
-                "sql",
-                "systemd",
-                "twig",
-                "typescript",
-                "xml",
-                "yaml"
-            ]
-            langs.forEach(lang => {
-                const info = bundledLanguagesInfo.find(i => i.aliases?.includes?.(lang) || i.id === lang);
+            const langsMap = {
+                bash: Bash,
+                c: C,
+                cpp: Cpp,
+                csv: Csv,
+                dockerfile: Dockerfile,
+                go: Go,
+                groovy: Groovy,
+                handlebars: Handlebars,
+                hcl: Hcl,
+                ini: Ini,
+                java: Java,
+                javascript: Javascript,
+                json: Json,
+                markdown: Markdown,
+                mermaid: Mermaid,
+                perl: Perl,
+                php: Php,
+                python: Python,
+                r: R,
+                ruby: Ruby,
+                rust: Rust,
+                scala: Scala,
+                sql: Sql,
+                systemd: Systemd,
+                twig: Twig,
+                typescript: Typescript,
+                xml: Xml,
+                yaml: Yaml,
+            };
+            Object.entries(langsMap).forEach(([langId, lang]) => {
+                const info = bundledLanguagesInfo.find(i => i.aliases?.includes?.(langId) || i.id === langId);
                 if (!info) {
-                    throw new Error(`[@nuxtjs/mdc] Could not find shiki language: ${lang}`);
+                    throw new Error(`[@nuxtjs/mdc] Could not find shiki language: ${langId}`);
                 }
-                const langImport = () => import(`shiki/langs/${info.id}.mjs`);
-                langsMap[info.id] = langImport;
                 for (const alias of info.aliases || []) {
-                    langsMap[alias] = langImport;
+                    langsMap[alias] = lang;
                 }
             });
 
