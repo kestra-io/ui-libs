962be71d8f18dc1a84ad5f4254ad394e8a23ae21
Bart Ledoux <bledoux@kestra.io>
2025-01-07 10:39:00 +0100

    add theme to published package

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import YamlUtils from "./utils/YamlUtils.js";
 import Utils from "./utils/Utils.js";
 import VueFlowUtils from "./utils/VueFlowUtils.js";
 
+import "./scss/theme-light.scss";
+import "./scss/theme-dark.scss";
+
 export {YamlUtils, Utils, VueFlowUtils};
 
 export * from "./components/index.js";
