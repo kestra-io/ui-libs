29aa6cfa53dbed839ef677f9f18d25567166b28d
Bart Ledoux <bledoux@kestra.io>
2025-01-08 22:07:04 +0100

    chore: update dependencies and improve package exports
    - Updated various dependencies in package-lock.json and package.json, including @nuxtjs/mdc, mermaid, shiki, and others to their latest versions.
    - Enhanced package exports in package.json to include types for the default import.
    - Added a new export for getMDCParser in src/index.ts.
    - Updated optional dependencies for esbuild and rollup packages to their latest versions.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export {YamlUtils} from "./utils/YamlUtils";
 export {cssVariable} from "./utils/global";
 export {default as Utils} from "./utils/Utils";
 export {default as VueFlowUtils} from "./utils/VueFlowUtils";
+export {default as getMDCParser} from "./composables/getMDCParser";
 
 import "./scss/theme-light.scss";
 import "./scss/theme-dark.scss";
