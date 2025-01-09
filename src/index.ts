f3527a425007b92d4d579ead362dbe00a524d592
Bart Ledoux <bledoux@kestra.io>
2025-01-09 09:52:56 +0100

    feat: add constants export

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export {cssVariable} from "./utils/global";
 export {default as Utils} from "./utils/Utils";
 export {default as VueFlowUtils} from "./utils/VueFlowUtils";
 export {default as getMDCParser} from "./composables/getMDCParser";
+export * from "./utils/constants";
 
 import "./scss/ks-theme-light.scss";
 import "./scss/ks-theme-dark.scss";
