b21162b88b143b65785a76d54ced7bc13c51e6ef
Piyush Bhaskar <102078527+Piyush-r-bhaskar@users.noreply.github.com>
2025-01-21 01:44:47 +0530

    feat: Add a new design of alerts according to alert level with icon. (#154)
    * chore(ui): Add a new design of alerts according to alert level with icon.
    
    This PR should be checked with 
    
    Relates to https://github.com/kestra-io/kestra/issues/5629
    
    * fix : remove inline svg and use vue icons

--- src/components/content/Alert.vue
+++ src/components/content/Alert.vue
@@ -1,32 +1,63 @@
 <template>
-    <div :class="'doc-alert alert alert-' + type" role="alert">
-        <slot />
+    <div :class="'doc-alert d-flex align-items-baseline alert alert-' + type" role="alert">
+        <div class="me-3">
+            <component :is="Icon"/>
+        </div>
+        <div class="d-flex flex-column">
+            <slot />
+        </div>
     </div>
 </template>
 
-<script setup lang="ts">
-    defineProps<{
-        type?: "info" | "warning" | "danger" | "success"
-    }>();
 
-    defineOptions({
-        name: "Alert",
-    });
+<script setup lang="ts">
+import { computed } from 'vue';
+import FileDocument from 'vue-material-design-icons/FileDocument.vue';
+import Information from 'vue-material-design-icons/AlertCircle.vue';
+import Warning from 'vue-material-design-icons/Alert.vue';
+import Close from 'vue-material-design-icons/CloseCircle.vue';
+
+const props = defineProps<{
+    type: "info" | "warning" | "danger" | "success"
+}>();
+
+defineOptions({
+    name: "Alert",
+});
+
+const Icon = computed(() => {
+    switch (props.type) {
+        case 'success':
+            return FileDocument;
+        case 'info':
+            return Information;
+        case 'warning':
+            return Warning;
+        case 'danger':
+            return Close;
+        default:
+            return Information;
+    }
+});
 </script>
 
 <style lang="scss" scoped>
     .doc-alert {
         --bs-alert-padding-x: 1rem;
-        --bs-alert-padding-y: 1rem;
         --bs-alert-margin-bottom: 1rem;
-
-        background-color: #18131f;
-        border: 1px solid #3a3c55;
-        padding: var(--bs-alert-padding-y) var(--bs-alert-padding-x);
+        border: 1px solid;
+        border-left-width: 5px !important;
+        padding: 0 var(--bs-alert-padding-x);
         margin-bottom: var(--bs-alert-margin-bottom);
 
         > * {
             margin: 0;
+        }
+
+        :deep(.material-design-icon__svg) {
+            width: 22px;
+            height: 22px;
+            bottom: -0.45em !important;
         }
     }
 </style>
