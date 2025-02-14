84c2b5772b954a5f2ae8ef8e8d01977c7fd5e604
Bart Ledoux <bledoux@kestra.io>
2025-02-14 16:11:55 +0100

    fix: use a nicer icon for static properties

--- src/components/plugins/CollapsibleProperties.vue
+++ src/components/plugins/CollapsibleProperties.vue
@@ -18,9 +18,9 @@
                     </template>
                     <template #buttonRight="{collapsed}">
                         <span class="d-flex flex-grow-1 align-items-center justify-content-between">
-                            <span class="d-flex gap-1">
+                            <span class="d-flex gap-2">
                                 <Tooltip v-if="showDynamic && !isDynamic(property)" class="d-flex" title="Non-dynamic">
-                                    <Cancel class="text-danger" />
+                                    <AlphaSBoxOutline class="text-danger" />
                                 </Tooltip>
                                 <Tooltip v-if="property['$beta']" class="d-flex" title="Beta">
                                     <AlphaBBox class="text-warning" />
@@ -63,7 +63,7 @@
     import PropertyDetail from "./PropertyDetail.vue";
     import ChevronUp from "vue-material-design-icons/ChevronUp.vue";
     import Alert from "vue-material-design-icons/Alert.vue";
-    import Cancel from "vue-material-design-icons/Cancel.vue";
+    import AlphaSBoxOutline from "vue-material-design-icons/AlphaSBoxOutline.vue";
     import AlphaBBox from "vue-material-design-icons/AlphaBBox.vue";
     import type {JSONSchema} from "./SchemaToHtml.vue";
     import EyeOutline from "vue-material-design-icons/EyeOutline.vue";
