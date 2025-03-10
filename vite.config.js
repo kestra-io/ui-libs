90847c88621eae850d6a7314b0c4cdb1fb0e193d
Bart Ledoux <bledoux@kestra.io>
2025-03-10 14:07:53 +0100

    build: externalize nuxt-mdc so build works fine later

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
         },
         rollupOptions: {
             external: [
+                "@nuxtjs/mdc/runtime",
                 "shiki",
                 "vue",
                 "vue-router",
@@ -49,6 +50,7 @@ export default defineConfig({
                     "@vue-flow/controls": "VueFlowControls",
                     "@popperjs/core": "PopperCore",
                     "moment-timezone": "MomentTimezone",
+                    "@nuxtjs/mdc/runtime": "NuxtMdcRuntime",
                 },
             }
         },
