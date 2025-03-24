da478a2e1a82b0228ffe7c41a0b227bb61075de7
MilosPaunovic <paun992@hotmail.com>
2025-03-24 15:45:46 +0100

    chore(ui): add anchors to plugin documentation

--- src/components/misc/Collapsible.vue
+++ src/components/misc/Collapsible.vue
@@ -1,6 +1,6 @@
 <template>
     <div :id="href" class="d-flex flex-column" :class="{'gap-2': !collapsed}">
-        <button @click="collapsed = !collapsed" class="d-flex align-items-center justify-content-between fw-bold gap-2 collapse-button" :class="{collapsed}" data-toggle="collapse" :data-target="'#' + href + '-body'" aria-expanded="false" :aria-controls="href + '-body'">
+        <button @click="handleClick" class="d-flex align-items-center justify-content-between fw-bold gap-2 collapse-button" :class="{collapsed}" data-toggle="collapse" :data-target="getHash" aria-expanded="false" :aria-controls="href + '-body'">
             <span class="d-flex gap-2 align-items-center"><component v-if="arrow" class="arrow" :is="collapsed ? MenuRight : MenuDown" />{{ clickableText }}<slot name="additionalButtonText" /></span>
             <span v-if="$slots.buttonRight" class="d-flex flex-grow-1">
                 <slot name="buttonRight" :collapsed="collapsed" />
@@ -15,10 +15,10 @@
 </template>
 
 <script setup lang="ts">
-    import {ref, watch} from "vue";
+    import {ref, watch, computed} from "vue";
     import MenuRight from "vue-material-design-icons/MenuRight.vue";
     import MenuDown from "vue-material-design-icons/MenuDown.vue";
-    import {useRoute} from "vue-router";
+    import {useRoute, useRouter} from "vue-router";
 
     const props = withDefaults(defineProps<{ href?: string, clickableText: string, arrow?: boolean, initiallyExpanded?: boolean }>(), {
         href: Math.random().toString(36).substring(2, 5),
@@ -29,6 +29,13 @@
     const collapsed = ref(!props.initiallyExpanded);
 
     const route = useRoute();
+    const router = useRouter();
+
+    const getHash = computed(() => `#${props.href}-body`);
+    const handleClick = () => {
+        collapsed.value = !collapsed.value;
+        router.replace({hash: collapsed.value ? "" : getHash.value});
+    };
 
     const emit = defineEmits(["expand"]);
 
@@ -37,7 +44,7 @@
             return route.hash;
         },
         routeHash => {
-            if (routeHash === "#" + props.href) {
+            if (routeHash === getHash.value) {
                 collapsed.value = false;
                 emit("expand");
             }
