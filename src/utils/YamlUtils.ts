e07ec654a9fda408d1658c66592dfa24fd89e8b0
Bart Ledoux <bledoux@kestra.io>
2025-05-07 16:06:14 +0200

    feat: add deletePluginDefaults

--- src/utils/YamlUtils.ts
+++ src/utils/YamlUtils.ts
@@ -1078,5 +1078,24 @@ export const YamlUtils = {
 
     pluginDefaults.value.items.splice(indexOfOldValue, 1, newItem);
     return yamlDoc.toString(TOSTRING_OPTIONS);
+  },
+
+  deletePluginDefaults(source: string, taskType: string) {
+    const yamlDoc = yaml.parseDocument(source) as any;
+    const pluginDefaults = this._extractPluginDefaults(yamlDoc) as yaml.Pair<Scalar, yaml.YAMLMap<{value: string}, {value: {items: any[]}}>> | undefined;
+    if (!pluginDefaults) {
+      return source;
+    }
+
+    let index = -1;
+    for(const def of pluginDefaults.value?.items ?? []){
+        index++;
+        if((def as any).get("type") === taskType){
+            pluginDefaults.value?.items.splice(index, 1);
+            break;
+        }
+    }
+
+    return yamlDoc.toString(TOSTRING_OPTIONS);
   }
 };
