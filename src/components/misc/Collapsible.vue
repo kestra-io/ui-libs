7dcfeed5b5f7619b25ace97a7cd3f372475ce13c
Bart Ledoux <bledoux@kestra.io>
2025-06-17 16:53:11 +0200

    fix: linting again

--- src/components/misc/Collapsible.vue
+++ src/components/misc/Collapsible.vue
@@ -1,8 +1,15 @@
 <template>
-    <div :id="href" class="d-flex flex-column" :class="{ 'gap-2': !collapsed }">
-        <button type="button" @click="handleClick"
-            class="d-flex align-items-center justify-content-between fw-bold gap-2 collapse-button" :class="{ collapsed }"
-            data-toggle="collapse" :data-target="getHash" aria-expanded="false" :aria-controls="href + '-body'">
+    <div :id="href" class="d-flex flex-column" :class="{'gap-2': !collapsed}">
+        <button
+            type="button"
+            @click="handleClick"
+            class="d-flex align-items-center justify-content-between fw-bold gap-2 collapse-button"
+            :class="{collapsed}"
+            data-toggle="collapse"
+            :data-target="getHash"
+            aria-expanded="false"
+            :aria-controls="href + '-body'"
+        >
             <span class="d-flex gap-2 align-items-center">
                 <component v-if="arrow" class="arrow" :is="collapsed ? MenuRight : MenuDown" />{{ clickableText }}
                 <slot name="additionalButtonText" />
@@ -11,7 +18,7 @@
                 <slot name="buttonRight" :collapsed="collapsed" />
             </span>
         </button>
-        <div v-if="$slots.content" :id="href + '-body'" class="collapsible-body" :class="{ collapsed }">
+        <div v-if="$slots.content" :id="href + '-body'" class="collapsible-body" :class="{collapsed}">
             <div>
                 <slot name="content" />
             </div>
@@ -20,57 +27,57 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch, computed } from "vue";
-import MenuRight from "vue-material-design-icons/MenuRight.vue";
-import MenuDown from "vue-material-design-icons/MenuDown.vue";
-import { useRoute, useRouter } from "vue-router";
+    import {ref, watch, computed} from "vue";
+    import MenuRight from "vue-material-design-icons/MenuRight.vue";
+    import MenuDown from "vue-material-design-icons/MenuDown.vue";
+    import {useRoute, useRouter} from "vue-router";
 
-const props = withDefaults(defineProps<{
-    href?: string,
-    clickableText: string,
-    arrow?: boolean,
-    initiallyExpanded?: boolean
-}>(), {
-    href: Math.random().toString(36).substring(2, 5),
-    arrow: true,
-    initiallyExpanded: false
-});
+    const props = withDefaults(defineProps<{
+        href?: string,
+        clickableText: string,
+        arrow?: boolean,
+        initiallyExpanded?: boolean
+    }>(), {
+        href: Math.random().toString(36).substring(2, 5),
+        arrow: true,
+        initiallyExpanded: false
+    });
 
-const collapsed = ref(!props.initiallyExpanded);
+    const collapsed = ref(!props.initiallyExpanded);
 
-const route = useRoute();
-const router = useRouter();
+    const route = useRoute();
+    const router = useRouter();
 
-const getHash = computed(() => `#${props.href}-body`);
-const handleClick = () => {
-    collapsed.value = !collapsed.value;
-    router.replace({ hash: collapsed.value ? "" : getHash.value });
-};
+    const getHash = computed(() => `#${props.href}-body`);
+    const handleClick = () => {
+        collapsed.value = !collapsed.value;
+        router.replace({hash: collapsed.value ? "" : getHash.value});
+    };
 
-const emit = defineEmits(["expand"]);
+    const emit = defineEmits(["expand"]);
 
-watch(
-    () => {
-        return route.hash;
-    },
-    routeHash => {
-        if (routeHash === getHash.value) {
-            collapsed.value = false;
-            emit("expand");
-        }
-    },
-    { immediate: true }
-);
+    watch(
+        () => {
+            return route.hash;
+        },
+        routeHash => {
+            if (routeHash === getHash.value) {
+                collapsed.value = false;
+                emit("expand");
+            }
+        },
+        {immediate: true}
+    );
 
-watch(
-    () => props.initiallyExpanded,
-    initiallyExpanded => {
-        if (initiallyExpanded) {
-            collapsed.value = !initiallyExpanded;
-        }
-    },
-    { immediate: true }
-);
+    watch(
+        () => props.initiallyExpanded,
+        initiallyExpanded => {
+            if (initiallyExpanded) {
+                collapsed.value = !initiallyExpanded;
+            }
+        },
+        {immediate: true}
+    );
 </script>
 
 <style scoped lang="scss">
