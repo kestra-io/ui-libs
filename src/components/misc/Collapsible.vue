9a2a17bef8fe506e2b2a29f7523fe5f3abf5d846
Bart Ledoux <bledoux@kestra.io>
2025-06-17 16:44:55 +0200

    remove details changes

--- src/components/misc/Collapsible.vue
+++ src/components/misc/Collapsible.vue
@@ -1,25 +1,21 @@
 <template>
-    <details :id="href" class="d-flex flex-column gap-2" @toggle="handleToggle" ref="collapsible">
-        <summary class="d-flex align-items-center justify-content-between fw-bold gap-2 collapse-button" :class="{collapsed}">
-            <span class="d-flex gap-2 align-items-center">
-                <component v-if="arrow" class="arrow" :is="collapsed ? MenuRight : MenuDown" />
-                {{ clickableText }}
-                <slot name="additionalButtonText" />
-            </span>
+    <div :id="href" class="d-flex flex-column" :class="{'gap-2': !collapsed}">
+        <button type="button" @click="handleClick" class="d-flex align-items-center justify-content-between fw-bold gap-2 collapse-button" :class="{collapsed}" data-toggle="collapse" :data-target="getHash" aria-expanded="false" :aria-controls="href + '-body'">
+            <span class="d-flex gap-2 align-items-center"><component v-if="arrow" class="arrow" :is="collapsed ? MenuRight : MenuDown" />{{ clickableText }}<slot name="additionalButtonText" /></span>
             <span v-if="$slots.buttonRight" class="d-flex flex-grow-1">
                 <slot name="buttonRight" :collapsed="collapsed" />
             </span>
-        </summary>
-        <div v-if="$slots.content" :id="href + '-body'" class="collapsible-body">
+        </button>
+        <div v-if="$slots.content" :id="href + '-body'" class="collapsible-body" :class="{collapsed}">
             <div>
                 <slot name="content" />
             </div>
         </div>
-    </details>
+    </div>
 </template>
 
 <script setup lang="ts">
-    import {ref, watch, computed, useTemplateRef, onMounted} from "vue";
+    import {ref, watch, computed} from "vue";
     import MenuRight from "vue-material-design-icons/MenuRight.vue";
     import MenuDown from "vue-material-design-icons/MenuDown.vue";
     import {useRoute, useRouter} from "vue-router";
@@ -35,18 +31,15 @@
         initiallyExpanded: false
     });
 
-    const collapsed = ref(true);
-    const collapsible = useTemplateRef("collapsible");
+    const collapsed = ref(!props.initiallyExpanded);
 
     const route = useRoute();
     const router = useRouter();
 
     const getHash = computed(() => `#${props.href}-body`);
-    const handleToggle = () => {
+    const handleClick = () => {
         collapsed.value = !collapsed.value;
-        router.replace({
-            hash: collapsed.value ? "" : getHash.value
-        });
+        router.replace({hash: collapsed.value ? "" : getHash.value});
     };
 
     const emit = defineEmits(["expand"]);
@@ -67,17 +60,12 @@
     watch(
         () => props.initiallyExpanded,
         initiallyExpanded => {
-            if (initiallyExpanded !== undefined) {
+            if (initiallyExpanded) {
                 collapsed.value = !initiallyExpanded;
             }
-        }
+        },
+        {immediate: true}
     );
-
-    onMounted(() => {
-        if(collapsible.value && props.initiallyExpanded !== undefined) {
-            collapsible.value.open = props.initiallyExpanded;
-        }
-    });
 </script>
 
 <style scoped lang="scss">
